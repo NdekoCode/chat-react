fb557fd26d7ac8cfcc4274a848c187173c44c103
NdekoCode <arickbulakali@gmail.com>
2022-10-17 14:45:08 +0200

    feat: message

--- src/Messages.jsx
+++ src/Messages.jsx
@@ -1,94 +1,71 @@
 import React from "react";
+import { useState } from "react";
+import Message from "./Message";
 
 const Messages = () => {
+  const [messages, setMessages] = useState([
+    {
+      id: Date.now(),
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti nam porro consequuntur voluptates, quibusdam laboriosam quia optio possimus quidem, recusandae quasi consequatur amet vitae aperiam! Consequatur quos quia quibusdam inventore.",
+      created_at: new Date().getTime(),
+    },
+    {
+      id: Date.now(),
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti nam porro consequuntur voluptates, quibusdam laboriosam quia optio possimus quidem, recusandae quasi consequatur amet vitae aperiam! Consequatur quos quia quibusdam inventore.",
+      created_at: new Date().getTime(),
+    },
+    {
+      id: Date.now(),
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti nam porro consequuntur voluptates, quibusdam laboriosam quia optio possimus quidem, recusandae quasi consequatur amet vitae aperiam! Consequatur quos quia quibusdam inventore.",
+      created_at: new Date().getTime(),
+    },
+    {
+      id: Date.now(),
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti nam porro consequuntur voluptates, quibusdam laboriosam quia optio possimus quidem, recusandae quasi consequatur amet vitae aperiam! Consequatur quos quia quibusdam inventore.",
+      created_at: new Date().getTime(),
+    },
+    {
+      id: Date.now(),
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti nam porro consequuntur voluptates, quibusdam laboriosam quia optio possimus quidem, recusandae quasi consequatur amet vitae aperiam! Consequatur quos quia quibusdam inventore.",
+      created_at: new Date().getTime(),
+    },
+    {
+      id: Date.now(),
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti nam porro consequuntur voluptates, quibusdam laboriosam quia optio possimus quidem, recusandae quasi consequatur amet vitae aperiam! Consequatur quos quia quibusdam inventore.",
+      created_at: new Date().getTime(),
+    },
+    {
+      id: Date.now(),
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti nam porro consequuntur voluptates, quibusdam laboriosam quia optio possimus quidem, recusandae quasi consequatur amet vitae aperiam! Consequatur quos quia quibusdam inventore.",
+      created_at: new Date().getTime(),
+    },
+    {
+      id: Date.now(),
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti nam porro consequuntur voluptates, quibusdam laboriosam quia optio possimus quidem, recusandae quasi consequatur amet vitae aperiam! Consequatur quos quia quibusdam inventore.",
+      created_at: new Date().getTime(),
+    },
+    {
+      id: Date.now(),
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti nam porro consequuntur voluptates, quibusdam laboriosam quia optio possimus quidem, recusandae quasi consequatur amet vitae aperiam! Consequatur quos quia quibusdam inventore.",
+      created_at: new Date().getTime(),
+    },
+  ]);
   return (
     <div className="flex flex-col h-full overflow-x-auto mb-4">
       <div className="flex flex-col h-full">
         <div className="grid grid-cols-12 gap-y-2">
-          <div className="col-start-1 col-end-8 p-3 rounded-lg">
-            <div className="flex flex-row items-center">
-              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                A
-              </div>
-              <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                <div>Hey How are you today?</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-start-1 col-end-8 p-3 rounded-lg">
-            <div className="flex flex-row items-center">
-              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                A
-              </div>
-              <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                <div>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
-                  ipsa commodi illum saepe numquam maxime asperiores voluptate
-                  sit, minima perspiciatis.
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-start-6 col-end-13 p-3 rounded-lg">
-            <div className="flex items-center justify-start flex-row-reverse">
-              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                A
-              </div>
-              <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                <div>I'm ok what about you?</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-start-6 col-end-13 p-3 rounded-lg">
-            <div className="flex items-center justify-start flex-row-reverse">
-              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                A
-              </div>
-              <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                <div>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing. ?
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-start-1 col-end-8 p-3 rounded-lg">
-            <div className="flex flex-row items-center">
-              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                A
-              </div>
-              <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                <div>Lorem ipsum dolor sit amet !</div>
-              </div>
-            </div>
-          </div>
-          <div className="col-start-6 col-end-13 p-3 rounded-lg">
-            <div className="flex items-center justify-start flex-row-reverse">
-              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                A
-              </div>
-              <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                <div>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing. ?
-                </div>
-                <div className="absolute text-xs bottom-0 right-0 -mb-5 mr-2 text-gray-500">
-                  Seen
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-start-1 col-end-8 p-3 rounded-lg">
-            <div className="flex flex-row items-center">
-              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                A
-              </div>
-              <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                <div>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Perspiciatis, in.
-                </div>
-              </div>
-            </div>
-          </div>
+          {messages.map((item, index) => (
+            <Message key={index} index={index} message={item} />
+          ))}
         </div>
       </div>
     </div>
